ddeba3ea8125a171b93b7a15ec446082def727fe
salma boussami <salmaboussami7@gmail.com>
2025-12-06 12:33:17 +0100

    static metode

--- src/main/java/com/sara/calculator/sara.java
+++ src/main/java/com/sara/calculator/sara.java
@@ -1,11 +1,11 @@
 package com.sara.calculator;
 
 public class sara {
-    public int add(int opG, int opD) {
+    public static int add(int opG, int opD) {
         return opG + opD;
     }
 
-    public int divide(int opG, int opD) {
+    public static  int divide(int opG, int opD) {
         return opG / opD;
     }
 }
